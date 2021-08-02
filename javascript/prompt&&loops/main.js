28fc7dc1436875fb9d8e87830e4e97692f8590f6
aschalegn <aschaley1@gmail.com>
2021-08-02 17:35:18 +0300

    guess the number if else & while loop

--- javascript/prompt&&loops/main.js
+++ javascript/prompt&&loops/main.js
@@ -1,18 +1,69 @@
-var userInput, chossenNumber = 3, maxGuess = 3;
+var userInput, chossenNumber = 3, guessCounter = 0;
 
-userInput = Number(prompt("guess the number"));
 
-if (userInput) {
-    if (userInput === chossenNumber && maxGuess) {
-        console.log("well done");
-    } else {
-        maxGuess--;
-        userInput = Number(prompt("guess the number"));
-        if (userInput === chossenNumber) {
-            console.log("well done");
-        } else {
 
-        }
+
+while (guessCounter < 3) {
+    userInput = Number(prompt("guess the number"));
+    guessCounter++;
+    if (userInput == chossenNumber) {
+        alert("woooow");
+        break;
     }
+    else {
 
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//! using while loop
+// while (guessCounter < 3) {
+//     userInput = Number(prompt("guess the number"));
+//     guessCounter++;
+//     if (userInput == chossenNumber) {
+//         alert("well done you did it in " + guessCounter);
+//         break;
+//     }
+// }
+
+//! using if else
+// userInput = Number(prompt("guess the number"));
+// guessCounter++;
+// if (userInput == chossenNumber ) {
+//     console.log("well done");
+// } else {
+//     userInput = Number(prompt("guess the number"));
+//     guessCounter++;
+//     if (userInput == chossenNumber ) {
+//         console.log("well done");
+//     }
+//     else {
+//         userInput = Number(prompt("guess the number"));
+//         guessCounter++;
+//         if (userInput == chossenNumber ) {
+//             console.log("well done");
+//         } else {
+//             console.log("sorry not the number");
+//         }
+//     }
+// }
+
+
+
+
--- javascript/vars/swap/main.js
+++ javascript/vars/swap/main.js
@@ -1,13 +1,25 @@
-var firstNumber = 56;
-var secondNumber = 24.5;
+var firstNumber = 30;
+var secondNumber = 20;
+
+
+console.log({ firstNumber, secondNumber });
+
+
+firstNumber = firstNumber + secondNumber; //50
+secondNumber = firstNumber - secondNumber; //30
+firstNumber = firstNumber - secondNumber; //20
 
 console.log({ firstNumber, secondNumber });
 
 
 
 
-firstNumber = firstNumber + secondNumber;
-secondNumber = firstNumber - secondNumber;
-firstNumber = firstNumber - secondNumber;
 
-console.log({ firstNumber, secondNumber });
+
+// var third = firstNumber;
+// firstNumber= secondNumber;
+// secondNumber= third;
+
+
+
+// console.log({ firstNumber, secondNumber });
