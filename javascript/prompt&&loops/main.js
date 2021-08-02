588c09ad487fe6df8eb48e8be1c285708aed330a
aschalegn <aschaley1@gmail.com>
2021-08-02 17:35:25 +0300

    guess the number if else & while loop

--- javascript/prompt&&loops/main.js
+++ javascript/prompt&&loops/main.js
@@ -1,47 +1,5 @@
 var userInput, chossenNumber = 3, guessCounter = 0;
 
-
-
-
-while (guessCounter < 3) {
-    userInput = Number(prompt("guess the number"));
-    guessCounter++;
-    if (userInput == chossenNumber) {
-        alert("woooow");
-        break;
-    }
-    else {
-
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//! using while loop
-// while (guessCounter < 3) {
-//     userInput = Number(prompt("guess the number"));
-//     guessCounter++;
-//     if (userInput == chossenNumber) {
-//         alert("well done you did it in " + guessCounter);
-//         break;
-//     }
-// }
-
 //! using if else
 // userInput = Number(prompt("guess the number"));
 // guessCounter++;
@@ -64,6 +22,15 @@ while (guessCounter < 3) {
 //     }
 // }
 
+//! using while loop
+while (guessCounter < 3) {
+    userInput = Number(prompt("guess the number"));
+    guessCounter++;
+    if (userInput == chossenNumber) {
+        alert("woooow");
+        break;
+    }
+}
 
 
 
